4ad835fd32fdd88cde744aab82a736618a0ef356
Richard Fussenegger <richard@fussenegger.info>
2015-01-25 11:53:56 +0100

    Formatting, make clean after configure, and fixed set e around service stop.

--- compile.sh
+++ compile.sh
@@ -36,14 +36,101 @@
 # LINK: http://richard.fussenegger.info/
 # -----------------------------------------------------------------------------
 
-# Print usage text.
+# Check return status of every command.
+set -e
+
+
+# ----------------------------------------------------------------------------- Variables
+
+
+# Used to collect additional modules that should be added to nginx.
+ADD_MODULES=''
+
+# For more information on shell colors and other text formatting see:
+# http://stackoverflow.com/a/4332530/1251219
+readonly RED=$(tput bold; tput setaf 1)
+readonly GREEN=$(tput bold; tput setaf 2)
+readonly YELLOW=$(tput bold; tput setaf 3)
+readonly NORMAL=$(tput sgr0)
+
+
+# ----------------------------------------------------------------------------- Functions
+
+
+# Add additional module to nginx.
 #
-# RETURN:
-#  0 - Printing successful.
-#  1 - Printing failed.
+# ARGS:
+#   $1 - The name of the directory within the source directory.
+add_module()
+{
+    ADD_MODULES="${ADD_MODULES}--add-module=${SOURCE_DIRECTORY}/${1} "
+}
+
+# Create directory and set nginx owner.
+#
+# ARGS:
+#   $1 - Absolute path to the directory.
+create_directory()
+{
+    mkdir --parents -- "${1}"
+    chmod -- 0755 "${1}"
+    chown -- "${USER}":"${GROUP}" "${1}"
+}
+
+# Download and extract given compressed tar archive.
+#
+# ARGS:
+#   $1 - The URL path to the file on the remote server.
+#   $2 - The name of the file.
+#   $3 - The version string.
+download_and_extract()
+{
+    # Use existing source files if version matches.
+    if [ -d "${2}-${3}" ]
+    then return 0
+    fi
+
+    # We don't know which version these files have, delete and retrieve again.
+    rm --force --recursive -- "${2}"
+
+    # Build archive name.
+    local ARCHIVE_NAME="${2}-${3}.tar.gz"
+
+    # Delete possibly left over archive.
+    rm --force -- "${ARCHIVE_NAME}"
+
+    # Download, extract, delete archive, simplify directory name by creating
+    # a symbolic link and make sure files belong to root user.
+    wget -- "${1}${ARCHIVE_NAME}"
+    tar --extract --file="${ARCHIVE_NAME}"
+    rm --force -- "${ARCHIVE_NAME}"
+    ln --symbolic -- "${2}-${3}" "${2}"
+    chown -- root:root "${SOURCE_DIRECTORY}/${2}"
+    chown --recursive -- root:root "${SOURCE_DIRECTORY}/${2}"
+}
+
+# Clone git repository (or pull changes if it already exists).
+#
+# ARGS:
+#   $1 - User name
+#   $2 - Repository name
+git_clone()
+{
+    if [ -d "${SOURCE_DIRECTORY}/${2}/.git" ]
+    then
+        cd -- "${SOURCE_DIRECTORY}/${2}"
+        git pull
+        cd -- "${SOURCE_DIRECTORY}"
+    else
+        rm --recursive --force -- "${SOURCE_DIRECTORY}/${1}"
+        git clone "https://github.com/${1}/${2}.git"
+    fi
+}
+
+# Print usage text.
 usage()
 {
-  cat << EOT
+    cat << EOT
 Usage: ${0##*/} [OPTION]... [TLS_LIBRARY_NAME]
 Compile and install nginx from source.
 
@@ -55,30 +142,26 @@ For complete documentation, see: README.md
 EOT
 }
 
+
+# -----------------------------------------------------------------------------
+
+
 # Check for possibly passed options.
 while getopts 'h' OPT
 do
-  case "${OPT}" in
-    h|[?]) usage && exit 0 ;;
-  esac
+    case "${OPT}" in
+        h|[?]) usage && exit 0 ;;
+    esac
 
-  # We have to remove found options from the input for later evaluations of
-  # passed arguments in subscripts that are not interested in these options.
-  shift $(( $OPTIND - 1 ))
+    # We have to remove found options from the input for later evaluations of
+    # passed arguments in subscripts that are not interested in these options.
+    shift $(( $OPTIND - 1 ))
 done
 
 # Remove possibly passed end of options marker.
 if [ "${1}" = "--" ]
-then
-  shift $(( $OPTIND - 1 ))
+then shift $(( $OPTIND - 1 ))
 fi
-
-# For more information on shell colors and other text formatting see:
-# http://stackoverflow.com/a/4332530/1251219
-readonly RED=$(tput bold; tput setaf 1)
-readonly GREEN=$(tput bold; tput setaf 2)
-readonly YELLOW=$(tput bold; tput setaf 3)
-readonly NORMAL=$(tput sgr0)
 
 # Include user configurable configuration.
 . "$(cd -- "$(dirname -- "${0}")"; pwd)"/config.sh
@@ -86,156 +169,69 @@ readonly NORMAL=$(tput sgr0)
 # Install dependencies.
 apt-get --yes -- install build-essential git
 
-# Used to collect additional modules that should be added to nginx.
-ADD_MODULES=''
-
-# Add additional module to nginx.
-#
-# ARGS:
-#   $1 - The name of the directory within the source directory.
-add_module()
-{
-  ADD_MODULES="${ADD_MODULES}--add-module=${SOURCE_DIRECTORY}/${1} "
-}
-
-# Create directory and set nginx owner.
-#
-# ARGS:
-#   $1 - Absolute path to the directory.
-create_directory()
-{
-  mkdir --parents -- "${1}"
-  chmod -- 0755 "${1}"
-  chown -- "${USER}":"${GROUP}" "${1}"
-}
-
-# Clone git repository (or pull changes if it already exists).
-#
-# ARGS:
-#   $1 - User name
-#   $2 - Repository name
-git_clone()
-{
-  if [ -d "${SOURCE_DIRECTORY}/${2}/.git" ]
-  then
-    cd -- "${SOURCE_DIRECTORY}/${2}"
-    git pull
-    cd -- "${SOURCE_DIRECTORY}"
-  else
-    rm --recursive --force -- "${SOURCE_DIRECTORY}/${1}"
-    git clone "https://github.com/${1}/${2}.git"
-  fi
-}
-
-# Download and extract given compressed tar archive.
-#
-# ARGS:
-#   $1 - The URL path to the file on the remote server.
-#   $2 - The name of the file.
-#   $3 - The version string.
-download_and_extract()
-{
-  # Use existing source files if version matches.
-  if [ -d "${2}-${3}" ]
-  then
-    return 0
-  fi
-
-  # We don't know which version these files have, delete and retrieve again.
-  rm --force --recursive -- "${2}"
-
-  # Build archive name.
-  local ARCHIVE_NAME="${2}-${3}.tar.gz"
-
-  # Delete possibly left over archive.
-  rm --force -- "${ARCHIVE_NAME}"
-
-  # Download, extract, delete archive, simplify directory name by creating
-  # a symbolic link and make sure files belong to root user.
-  wget -- "${1}${ARCHIVE_NAME}"
-  tar --extract --file="${ARCHIVE_NAME}"
-  rm --force -- "${ARCHIVE_NAME}"
-  ln --symbolic -- "${2}-${3}" "${2}"
-  chown -- root:root "${SOURCE_DIRECTORY}/${2}"
-  chown --recursive -- root:root "${SOURCE_DIRECTORY}/${2}"
-}
-
-# Check return status of every command.
-set -e
-
-printf -- 'Installing nginx %s ...\n' "${YELLOW}${NGINX_VERSION}${NORMAL}"
-
 # Make sure we operate from the correct directory.
 cd -- "${SOURCE_DIRECTORY}"
 
-if [ $NGINX_VERSION = false ]
+# Download nginx source files.
+if [ ${NGINX_VERSION} = false ]
 then
-  git_clone Fleshgrinder nginx
+    printf -- 'Installing nginx from repository ...\n'
+    git_clone Fleshgrinder nginx
 else
-  download_and_extract 'http://nginx.org/download/' 'nginx' "${NGINX_VERSION}"
+    printf -- 'Installing nginx %s ...\n' "${YELLOW}${NGINX_VERSION}${NORMAL}"
+    download_and_extract 'http://nginx.org/download/' 'nginx' "${NGINX_VERSION}"
 fi
 
+# Download PCRE source files.
 download_and_extract 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/' 'pcre' "${PCRE_VERSION}"
 
+# Download TLS library source files.
 case "${TLS_LIBRARY_NAME}" in
-  openssl)
-    download_and_extract 'http://www.openssl.org/source/' 'openssl' "${TLS_LIBRARY_VERSION}"
-  ;;
+    openssl)
+        download_and_extract 'http://www.openssl.org/source/' 'openssl' "${TLS_LIBRARY_VERSION}"
+    ;;
 
-  boringssl)
-#     if [ -d 'boringssl' ]
-#     then
-#      cd -- boringssl
-#      git pull
-#      cd -- ..
-#    else
-#      git clone 'https://boringssl.googlesource.com/boringssl'
-#    fi
-    printf '%sTODO:%s boringssl' "${YELLOW}" "${NORMAL}" >&2
-    exit 1
-  ;;
+    boringssl)
+        printf -- '%sTODO:%s boringssl' "${YELLOW}" "${NORMAL}" >&2
+        exit 1
+    ;;
 
-  libressl)
-    printf '%sTODO:%s libressl' "${YELLOW}" "${NORMAL}" >&2
-    exit 1
-  ;;
+    libressl)
+        printf -- '%sTODO:%s libressl' "${YELLOW}" "${NORMAL}" >&2
+        exit 1
+    ;;
 
-  *)
-    printf '[%sfail%s] Unsupported TLS library `%s` specified, check your configuration.\n' \
-      "${RED}" "${NORMAL}" "${TLS_LIBRARY_NAME}" >&2
-    exit 1
-  ;;
+    *)
+        printf -- '[%sfail%s] Unsupported TLS library `%s` specified, check your configuration.\n' \
+            "${RED}" "${NORMAL}" "${TLS_LIBRARY_NAME}" >&2
+        exit 1
+;;
 esac
 
-# Ensure zlib is up to date.
-if [ -d zlib ]
-then
-  cd -- "${SOURCE_DIRECTORY}/zlib"
-  git pull
-  cd -- "${SOURCE_DIRECTORY}"
-else
-  git clone 'https://github.com/madler/zlib.git'
-fi
+# Download zlib source files.
+git_clone madler zlib
 
+# Download accept language module source files.
 if [ ${ACCEPT_LANGUAGE} = true ]
 then
-  git_clone Fleshgrinder nginx_accept_language_module
+    git_clone Fleshgrinder nginx_accept_language_module
 fi
 
+# Download Google PageSpeed source files and objects.
 if [ ${GOOGLE_PAGESPEED} = true ]
 then
-  git_clone pagespeed ngx_pagespeed
-  if [ !-d "${SOURCE_DIRECTORY}/ngx_pagespeed/psol" ]
-  then
-    cd -- "${SOURCE_DIRECTORY}/ngx_pagespeed"
-    wget -- "https://dl.google.com/dl/page-speed/psol/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
-    tar --extract --file="${SOURCE_DIRECTORY}/ngx_pagespeed/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
-    rm --force -- "${SOURCE_DIRECTORY}/ngx_pagespeed/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
-    cd -- "${SOURCE_DIRECTORY}"
-    chown --recursive -- root:root "${SOURCE_DIRECTORY}/ngx_pagespeed/psol"
-    chmod --recursive -- 0755 "${SOURCE_DIRECTORY}/ngx_pagespeed/psol"
-    find "${SOURCE_DIRECTORY}/ngx_pagespeed/psol" -type f -exec chmod 644 {} \;
-  fi
+    git_clone pagespeed ngx_pagespeed
+    if [ !-d "${SOURCE_DIRECTORY}/ngx_pagespeed/psol" ]
+    then
+        cd -- "${SOURCE_DIRECTORY}/ngx_pagespeed"
+        wget -- "https://dl.google.com/dl/page-speed/psol/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
+        tar --extract --file="${SOURCE_DIRECTORY}/ngx_pagespeed/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
+        rm --force -- "${SOURCE_DIRECTORY}/ngx_pagespeed/${GOOGLE_PAGESPEED_VERSION}.tar.gz"
+        cd -- "${SOURCE_DIRECTORY}"
+        chown --recursive -- root:root "${SOURCE_DIRECTORY}/ngx_pagespeed/psol"
+        chmod --recursive -- 0755 "${SOURCE_DIRECTORY}/ngx_pagespeed/psol"
+        find "${SOURCE_DIRECTORY}/ngx_pagespeed/psol" -type f -exec chmod 644 {} \;
+    fi
 fi
 
 # Configure, compile, and install nginx.
@@ -245,48 +241,50 @@ CXXFLAGS="${CFLAGS}" \
 CPPFLAGS="${CFLAGS}" \
 LDFLAGS='' \
 ./configure \
-  --user="${USER}" \
-  --group="${GROUP}" \
-  --prefix='/usr/local' \
-  --sbin-path='/usr/local/sbin' \
-  --conf-path='/etc/nginx/nginx.conf' \
-  --pid-path='/run/nginx.pid' \
-  --lock-path='/var/lock/nginx.lock' \
-  --error-log-path='/var/log/nginx/error.log' \
-  --http-client-body-temp-path='/var/nginx/uploads' \
-  --http-fastcgi-temp-path='/var/nginx/fastcgi' \
-  --http-log-path='/var/log/nginx/access.log' \
-  --with-cc-opt="${CFLAGS}" \
-  --with-ld-opt="${LDFLAGS}" \
-  --with-debug \
-  --with-ipv6 \
-  --with-http_gzip_static_module \
-  --with-http_ssl_module \
-  --with-http_spdy_module \
-  --with-openssl-opt='-DOPENSSL_NO_HEARTBEATS enable-ec_nistp_64_gcc_128 no-rc2 no-rc4 no-rc5 no-md2 no-md4 no-ssl2 no-ssl3 no-krb5 no-hw no-engines' \
-  --with-openssl="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
-  --with-md5="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
-  --with-md5-asm \
-  --with-sha1="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
-  --with-sha1-asm \
-  --with-pcre="${SOURCE_DIRECTORY}/pcre" \
-  --with-pcre-jit \
-  --with-zlib="${SOURCE_DIRECTORY}/zlib" \
-  ${ADD_MODULES} \
-  --without-http_access_module \
-  --without-http_auth_basic_module \
-  --without-http_autoindex_module \
-  --without-http_empty_gif_module \
-  --without-http_geo_module \
-  --without-http_memcached_module \
-  --without-http_proxy_module \
-  --without-http_referer_module \
-  --without-http_scgi_module \
-  --without-http_split_clients_module \
-  --without-http_ssi_module \
-  --without-http_upstream_ip_hash_module \
-  --without-http_userid_module \
-  --without-http_uwsgi_module
+    --user="${USER}" \
+    --group="${GROUP}" \
+    --prefix='/usr/local' \
+    --sbin-path='/usr/local/sbin' \
+    --conf-path='/etc/nginx/nginx.conf' \
+    --pid-path='/run/nginx.pid' \
+    --lock-path='/var/lock/nginx.lock' \
+    --error-log-path='/var/log/nginx/error.log' \
+    --http-client-body-temp-path='/var/nginx/uploads' \
+    --http-fastcgi-temp-path='/var/nginx/fastcgi' \
+    --http-log-path='/var/log/nginx/access.log' \
+    --with-cc-opt="${CFLAGS}" \
+    --with-ld-opt="${LDFLAGS}" \
+    --with-debug \
+    --with-ipv6 \
+    --with-http_gzip_static_module \
+    --with-http_ssl_module \
+    --with-http_spdy_module \
+    --with-openssl-opt='-DOPENSSL_NO_HEARTBEATS enable-ec_nistp_64_gcc_128 no-rc2 no-rc4 no-rc5 no-md2 no-md4 no-ssl2 no-ssl3 no-krb5 no-hw no-engines' \
+    --with-openssl="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
+    --with-md5="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
+    --with-md5-asm \
+    --with-sha1="${SOURCE_DIRECTORY}/${TLS_LIBRARY_NAME}" \
+    --with-sha1-asm \
+    --with-pcre="${SOURCE_DIRECTORY}/pcre" \
+    --with-pcre-jit \
+    --with-zlib="${SOURCE_DIRECTORY}/zlib" \
+    ${ADD_MODULES} \
+    --without-http_access_module \
+    --without-http_auth_basic_module \
+    --without-http_autoindex_module \
+    --without-http_empty_gif_module \
+    --without-http_geo_module \
+    --without-http_memcached_module \
+    --without-http_proxy_module \
+    --without-http_referer_module \
+    --without-http_scgi_module \
+    --without-http_split_clients_module \
+    --without-http_ssi_module \
+    --without-http_upstream_ip_hash_module \
+    --without-http_userid_module \
+    --without-http_uwsgi_module
+
+make clean
 make
 
 # Create the directories for temporary data.
@@ -295,22 +293,23 @@ create_directory /var/nginx/fastcgi
 
 if [ ! -f /etc/init.d/nginx ]
 then
-  # Download SysVinit compliant script and ensure correct permissions and owner.
-  wget --output-document=/etc/init.d/nginx 'https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/nginx'
-  chmod -- 0775 /etc/init.d/nginx
-  chown -- root:root /etc/init.d/nginx
+    # Download SysVinit compliant script and ensure correct permissions and owner.
+    wget --output-document=/etc/init.d/nginx 'https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/nginx'
+    chmod -- 0775 /etc/init.d/nginx
+    chown -- root:root /etc/init.d/nginx
 
-  # Ensure nginx is started upon system startup.
-  update-rc.d nginx defaults
+    # Ensure nginx is started upon system startup.
+    update-rc.d nginx defaults
 fi
 
-set -e
-service nginx stop 2>/dev/null
+# Try to stop the service in case it is running.
 set +e
+service nginx stop 2>/dev/null
+set -e
 
+# Install the newly compiled nginx and start.
 make install
 service nginx start
-make clean
 
-printf '[%sok%s] Installation finished.\n' "${GREEN}" "${NORMAL}"
+printf -- '[%sok%s] Installation finished.\n' "${GREEN}" "${NORMAL}"
 exit 0
