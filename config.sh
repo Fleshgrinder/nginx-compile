f391452c1a281735ad485cd66cb6df4be719a1c9
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2015-12-06 18:20:29 +0100

    Updated scripts to new nginx repo layout.

--- config.sh
+++ config.sh
@@ -1,38 +1,12 @@
 #!/bin/sh
 
 # ------------------------------------------------------------------------------
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or # distribute
-# this software, either in source code form or as a compiled binary, for any
-# purpose, commercial or non-commercial, and by any means.
-#
-# In jurisdictions that recognize copyright laws, the author or authors of this
-# software dedicate any and all copyright interest in the software to the public
-# domain. We make this dedication for the benefit of the public at large and to
-# the detriment of our heirs and successors. We intend this dedication to be an
-# overt act of relinquishment in perpetuity of all present and future rights to
-# this software under copyright law.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# For more information, please refer to <http://unlicense.org>
-# ------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------
 # Configuration file for nginx-compile scripts.
 #
-# AUTHOR: Richard Fussenegger <richard@fussenegger.info>
-# COPYRIGHT: Copyright (c) 2013-15 Richard Fussenegger
-# LICENSE: http://unlicense.org/ PD
-# LINK: http://richard.fussenegger.info/
+# @author Richard Fussenegger <fleshgrinder@users.noreply.github.com>
+# @copyright 2013-15 Richard Fussenegger
+# @license http://unlicense.org/ PD
 # ------------------------------------------------------------------------------
-
 
 # ------------------------------------------------------------------------------
 #                                                           COMMON CONFIGURATION
@@ -47,7 +21,7 @@ readonly NGINX_VERSION=false
 readonly NGINX_ONTHEFLY_UPGRADE=true
 
 # The version string of the PCRE release that should be installed.
-readonly PCRE_VERSION='8.36'
+readonly PCRE_VERSION='8.37'
 
 # The name of the SSL/TLS library nginx should be compiled against.
 #
@@ -57,13 +31,13 @@ readonly PCRE_VERSION='8.36'
 readonly TLS_LIBRARY_NAME="${1:-openssl}"
 
 # The version string of the SSL/TLS library release that should be installed.
-readonly TLS_LIBRARY_VERSION='1.0.2a'
+readonly TLS_LIBRARY_VERSION='1.0.2e'
 
 # Determines if the Accept-Language module should be compiled or not.
 readonly ACCEPT_LANGUAGE=true
 
 # Determines if the Google PageSpeed module should be compiled or not.
-readonly GOOGLE_PAGESPEED=true
+readonly GOOGLE_PAGESPEED=false
 
 # The version string of the Google PageSpeed library.
 readonly GOOGLE_PAGESPEED_VERSION='1.9.32.3'
@@ -87,7 +61,8 @@ readonly NGINX_INITD=true
 # the configure call at the bottom of this script.
 readonly NGINX_PREFIX='/usr/local'
 readonly NGINX_SBIN_PATH="${NGINX_PREFIX}/sbin"
-readonly NGINX_CONF_PATH='/etc/nginx'
+readonly NGINX_CONF_PATH="${NGINX_PREFIX}/etc/nginx"
+readonly NGINX_CONF_SUFFIX='ngx'
 readonly NGINX_PID_PATH='/run/nginx.pid'
 readonly NGINX_LOCK_PATH='/run/nginx.lock'
 readonly NGINX_LOG_PATH='/var/log/nginx'
